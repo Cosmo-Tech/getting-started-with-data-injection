d8eb0dad5fd2c2d778697b403da72c682c74b485
Clément Rezvoy <clement.rezvoy@gmail.com>
2021-03-23 11:15:42 +0100

    try automatic extension install

--- arm/uploadTwinModels.sh
+++ arm/uploadTwinModels.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -sLo "$MODEL_FILE" "$MODEL_URI"
-az extension add --name "azure-iot"
+az config set extension.use_dynamic_install=yes_without_prompt
 az dt model create -n "$MODEL_INSTANCES_NAME" --models "$MODEL_FILE"
