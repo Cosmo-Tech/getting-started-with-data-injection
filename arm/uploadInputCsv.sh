a68d4ee7e4eae8bc099462a90aeeb40fa3618a8b
Clément Rezvoy <clement.rezvoy@gmail.com>
2021-03-27 14:00:53 +0100

    Add brewery input files

--- arm/uploadInputCsv.sh
+++ arm/uploadInputCsv.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-curl -sLo "$BLOB_NAME" "$INPUT_URI"
-az storage blob upload \
-   --container-name "$CONTAINER_NAME" \
-   --file "$BLOB_NAME" \
-   --name "$BLOB_NAME" \
-   --account-name "$STORAGE_ACCOUNT" \
-   --account-key "$STORAGE_KEY"
+function upload_file {
+    local input_url="$1"
+    local filename="$2"
+    local storage_account="$3"
+    local storage_key="$4"
+    local container_name="$5"
+    curl -sLo "$filename" "$input_url/$filename"
+    az storage blob upload \
+       --container-name "$container_name" \
+       --file "$filename" \
+       --name "$filename" \
+       --account-name "$storage_account" \
+       --account-key "$storage_key"
+}
+
+upload_file "$INPUT_URL" "$BARS_DATASET" "$STORAGE_ACCOUNT" "$STORAGE_KEY" "$CONTAINER_NAME"
+upload_file "$INPUT_URL" "$CUSTOMERS_DATASET" "$STORAGE_ACCOUNT" "$STORAGE_KEY" "$CONTAINER_NAME"
+upload_file "$INPUT_URL" "$CUSTOMER_RELATIONSHIPS_DATASET" "$STORAGE_ACCOUNT" "$STORAGE_KEY" "$CONTAINER_NAME"
+
